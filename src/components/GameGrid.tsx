ba6998307dc261b9d7099f934593b88ebe78f769
afshin <shahinof10009@gmail.com>
2023-11-07 02:34:30 +0330

    using useGame hook for fetching games from api

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,28 +1,7 @@
-import { useEffect, useState } from "react";
-import api from "../services/rawg-api-client";
-import type Game from "../schemas/game.schema";
-
-type GamesResponse = {
-  results: Game[];
-  count: number;
-  next?: string;
-  previous?: string;
-};
+import useGames from "../hooks/useGames";
 
 const GameGrid = () => {
-  const [games, setGames] = useState<Game[]>([]);
-  const [error, setError] = useState("");
-
-  useEffect(() => {
-    api
-      .get<GamesResponse>("/games")
-      .then((res) => {
-        setGames(res.data.results);
-      })
-      .catch((e) => {
-        setError(e?.message ?? "error occured");
-      });
-  }, []);
+  const { error, games } = useGames();
 
   return (
     <>
