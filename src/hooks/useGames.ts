0ed9d4fe2553624d04dead35602c916d85d8890a
afshin <shahinof10009@gmail.com>
2023-11-07 20:27:44 +0330

    `useGames` hook expose `loading` state

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -9,24 +9,31 @@ type GamesResponse = {
   previous?: string;
 };
 
-const useGames = (): { games: Game[]; error: string } => {
+const useGames = (): { games: Game[]; error: string; loading: Boolean } => {
   const [games, setGames] = useState<Game[]>([]);
   const [error, setError] = useState("");
+  const [loading, setLoading] = useState<Boolean>(false);
 
   useEffect(() => {
+    setLoading(true);
     api
       .get<GamesResponse>("/games")
       .then((res) => {
         setGames(res.data.results);
+
+        setLoading(false);
       })
       .catch((e) => {
         setError(e?.message ?? "error occured");
+
+        setLoading(false);
       });
   }, []);
 
   return {
     games: games,
     error: error,
+    loading: loading,
   };
 };
 
