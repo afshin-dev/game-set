0e72d12d1d98b1334d07baca1b788a144bf100c1
afshin <shahinof10009@gmail.com>
2023-11-07 17:55:50 +0330

    adding `PlatformIconList` to `GameCard` component

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,5 +1,7 @@
-import { Card, CardBody, Heading, Image } from "@chakra-ui/react";
+import { Card, CardBody, Heading, Image, Text } from "@chakra-ui/react";
 import type Game from "../schemas/game.schema";
+import PlatformIconList from "./PlatformIconList";
+import Platform from "../schemas/platform.schema";
 
 interface Props {
   game: Game;
@@ -7,11 +9,23 @@ interface Props {
 }
 
 const GameCard = ({ game }: Props) => {
+  game.parent_platforms_to_platform_array = (
+    pp: {
+      platform: Platform;
+    }[]
+  ): Platform[] => {
+    return pp.map((o) => o.platform);
+  };
   return (
     <>
       <Card borderRadius={10} overflow={"hidden"}>
         <Image src={game.background_image}></Image>
         <CardBody>
+          <PlatformIconList
+            platforms={game.parent_platforms_to_platform_array(
+              game.parent_platforms
+            )}
+          ></PlatformIconList>
           <Heading fontSize={"2xl"}>{game.name}</Heading>
         </CardBody>
       </Card>
