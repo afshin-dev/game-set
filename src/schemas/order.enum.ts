eec724ea5f2acc27496994b361f31ed915e5d2d8
afshin <shahinof10009@gmail.com>
2023-11-08 17:05:12 +0330

    added `order` enum

---
+++ src/schemas/order.enum.ts
@@ -0,0 +1,19 @@
+// A at the end = ascending
+// D at the end = descending
+enum Order {
+  nameD = "-name",
+  nameA = "name",
+  releasedD = "-released",
+  releasedA = "released",
+  addedD = "-added",
+  addedA = "added",
+  createdD = "-created",
+  createdA = "created",
+  updatedD = "-updated",
+  updatedA = "updated",
+  ratingD = "-rating",
+  ratingA = "rating",
+  metacriticD = "-metacritic",
+  metacriticA = "metacritic",
+}
+export default Order;
